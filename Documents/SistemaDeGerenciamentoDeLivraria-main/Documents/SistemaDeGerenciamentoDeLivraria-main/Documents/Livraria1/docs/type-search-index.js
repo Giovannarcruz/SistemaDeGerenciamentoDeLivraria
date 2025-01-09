630a2473740d0553884d4e3d0ca33cd35ec59cd1
Giovannarcruz <giovann4.ro@gmail.com>
2025-01-09 19:22:01 -0300

    Delete Documents/SistemaDeGerenciamentoDeLivraria-main/Documents directory

--- Documents/SistemaDeGerenciamentoDeLivraria-main/Documents/SistemaDeGerenciamentoDeLivraria-main/Documents/Livraria1/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"giovanna.projeto.livraria1.dao","l":"GeneroDAO"},{"p":"giovanna.projeto.livraria1.dao","l":"LivroDAO"},{"p":"giovanna.projeto.livraria1.dao","l":"LivroSimilaresDAO"}];updateSearchResults();
